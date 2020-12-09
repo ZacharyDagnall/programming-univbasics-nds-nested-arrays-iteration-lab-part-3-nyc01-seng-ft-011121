d63d91a7dbd3b41a87ab8faf1c78c6819bc8183d
Zachary R Dagnall <zachary.dagnall@gmail.com>
2020-12-09 16:21:29 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ end
 
 def join_string_1D(arr) #some mild flattening
   sent = ""
-  for j in arr.length do
+  for j in 0...arr.length do
     if  arr[j].class == String
       sent +=arr[j]
     end
