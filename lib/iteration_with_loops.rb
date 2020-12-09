8d239ad6fb2c8ae513730a7176e8ce0f48432376
Zachary R Dagnall <zachary.dagnall@gmail.com>
2020-12-09 16:23:50 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,17 +1,21 @@
 def join_nested_strings(src)
   sentence = ""
   for i in 0...src.length do
-     sentence += " " + join_string_1D(src[i])
+    for j in 0...src[i].length do
+      if src[i][j].class == String
+        sentence += " " + src[i][j]
+      end
+    end
   end
   sentence
 end
 
-def join_string_1D(arr) #some mild flattening
-  sent = ""
-  for j in 0...arr.length do
-    if  arr[j].class == String
-      sent += " " + arr[j]
-    end
-  end 
-  sent
-end
+# def join_string_1D(arr) #some mild flattening
+#   sent = ""
+#   for j in 0...arr.length do
+#     if  arr[j].class == String
+#       sent += " " + arr[j]
+#     end
+#   end 
+#   sent
+# end
