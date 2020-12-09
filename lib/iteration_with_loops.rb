63e1fa72fdda603fa36552fe46bd993e43b0ec60
Zachary R Dagnall <zachary.dagnall@gmail.com>
2020-12-09 16:24:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,13 +9,3 @@ def join_nested_strings(src)
   end
   sentence
 end
-
-# def join_string_1D(arr) #some mild flattening
-#   sent = ""
-#   for j in 0...arr.length do
-#     if  arr[j].class == String
-#       sent += " " + arr[j]
-#     end
-#   end 
-#   sent
-# end
